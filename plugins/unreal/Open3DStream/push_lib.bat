1f14d6ee16d616cdfcbc3f211d2c3d65925b2ea6
Al <amacleod@moves.com>
2021-01-20 10:37:24 -0800

    fixed bug in subscriber that would cause a crash

--- plugins/unreal/Open3DStream/push_lib.bat
+++ plugins/unreal/Open3DStream/push_lib.bat
@@ -1,9 +1,10 @@
+@ECHO OFF
 REM SET DST=E:\UnrealProjects\MyProject2\plugins\Open3DStream
-SET DST=C:\Users\Al\Downloads\Open3dStream1\Plugins\Open3DStream
 SET DST=C:\Users\Al\Documents\Unreal Projects\Open3dStream1\Plugins\Open3DStream
 
-IF EXIST %DST%\Open3DStream.uplugin POK
+IF EXIST "%DST%\Open3DStream.uplugin" GOTO POK
 ECHO Invalid destination dir: %DST%
+PAUSE
 EXIT /B
 
 :POK
@@ -24,6 +25,9 @@ IF NOT EXIST "%DST%"\lib\include\nng MKDIR "%DST%"\lib\include\flatbuffers
 
 COPY "%~DP0..\..\..\build\src\RelWithDebInfo\*.lib"  "%DST%"\lib
 COPY "%~DP0..\..\..\build\src\RelWithDebInfo\*.bsc"  "%DST%"\lib
+
+COPY "%~DP0..\..\..\thirdparty\build\nng\RelWithDebInfo\nng.lib" "%DST%"\lib
+COPY "%~DP0..\..\..\thirdparty\build\flatbuffers\RelWithDebInfo\flatbuffers.lib" "%DST%"\lib
 
 COPY "%~DP0..\..\..\src\o3ds\*.h" "%DST%\lib\include\o3ds\"
 XCOPY /S /I /Y "%~DP0..\..\..\include\nng" "%DST%\lib\include\nng\"
