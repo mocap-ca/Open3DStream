802fdc5378788fdeb0921146c86a74b2b4443f82
Al Macleod Stage <al@peeldev.com>
2023-01-19 09:28:12 -0800

    removed defines for versions

--- src/o3ds/o3ds.cpp
+++ src/o3ds/o3ds.cpp
@@ -1,11 +1,8 @@
 #include "o3ds/o3ds.h"
 
-#define PQ(x) #x
-#define PQUOTE(x) PQ(x)
-
-const char* O3DS::version       = PQUOTE(O3DS_VERSION);
-const int   O3DS::version_major = O3DS_VERSION_MAJOR;
-const int   O3DS::version_minor = O3DS_VERSION_MINOR;
-const int   O3DS::version_patch = O3DS_VERSION_PATCH;
+const char* O3DS::version       = "0.1.0";
+const int   O3DS::version_major = 0;
+const int   O3DS::version_minor = 1;
+const int   O3DS::version_patch = 0;
 
 const char* O3DS::getVersion() { return O3DS::version; }
