aa2d27fcd52d9b3d1d5f868f91ee5ca584fa07da
Matin Lotfaliei <matinlotfali@gmail.com>
2024-09-30 16:50:26 -0700

    Find Mobu and Unreal install locations using windows registry

--- package.py
+++ package.py
@@ -5,6 +5,7 @@ import sys
 import json
 import re
 import subprocess
+import winreg
 
 """ creats a zip file release """
 
@@ -19,15 +20,12 @@ def get_version():
     
     
 
-def build_ue(plugin, ue_base, version):
+def build_ue(plugin, ue_base, version, ue_install_path):
 
     out = ue_base + "\\" + version + "\\PeelVCam"
-    
-    exe = r"d:\UnrealEngine\UE_" + version + "\Engine\Build\BatchFiles\RunUAT.bat"
-    
-    if not os.path.isfile(exe) :
-        exe = r"d:\EpicGames\UE_" + version + "\Engine\Build\BatchFiles\RunUAT.bat"
-      
+
+    exe = rf"{ue_install_path}\Engine\Build\BatchFiles\RunUAT.bat"
+
     if not os.path.isfile(exe) :      
         raise RuntimeError("Could not find unreal: " + version)
     
@@ -39,13 +37,15 @@ def build_ue(plugin, ue_base, version):
             "-Rocket",
             "-VS2022"
             ]
-            
-    pr = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    out, error = pr.communicate()
-    print(out.decode("utf8"), error.decode('utf8'))
-    
-    if pr.returncode != 0:
-        raise RuntimeError("Plugin Build Failed")
+
+    try:
+        pr = subprocess.Popen(cmd, shell=True)
+        pr.wait()
+
+        if pr.returncode != 0:
+            raise RuntimeError("Plugin Build Failed")
+    except Exception as ex:
+        print(ex)
 
 
 version_input = get_version()
@@ -69,23 +69,27 @@ for d, _, f in os.walk(out_dir):
         out = os.path.join(base, each_file)
         print(out)
         fp.write(src, out)
+fp.close()
 
 # Unreal    
-# unreal_src_plugin = os.path.abspath("plugins/unreal/Open3DStream/Open3DStream.uplugin")
+unreal_src_plugin = os.path.abspath("plugins/unreal/Open3DStream/Open3DStream.uplugin")
 unreal_dst_base = os.path.join(cwd, "plugins", "unreal")
 
-unreal_src_plugin = r"D:\P4_PD\o3ds\Plugins\Open3DStream\Open3DStream.uplugin"
-
-build_ue(unreal_src_plugin, unreal_dst_base, "5.0")
-build_ue(unreal_src_plugin, unreal_dst_base, "5.1")
-build_ue(unreal_src_plugin, unreal_dst_base, "5.2")
-build_ue(unreal_src_plugin, unreal_dst_base, "5.3")
-
-
+ue_registry_prefix_path = r'SOFTWARE\EpicGames\Unreal Engine'
+ue_registry_prefix_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, ue_registry_prefix_path, 0, winreg.KEY_READ)
+ue_version_index = 0
+while True:
+    try:
+        ue_version = winreg.EnumKey(ue_registry_prefix_key, ue_version_index)
+        ue_version_key = winreg.OpenKey(ue_registry_prefix_key, ue_version, 0, winreg.KEY_READ)
+        ue_path, _ = winreg.QueryValueEx(ue_version_key, "InstalledDirectory")
+        build_ue(unreal_src_plugin, unreal_dst_base, ue_version, ue_path)
+        ue_version_index = ue_version_index + 1
+    except OSError:
+        break
 
 raise RuntimeError("Here")
-fp.close()    
-        
+
 #installer
 
 nsis = "C:/Program Files (x86)/NSIS/makensis.exe"
